b106688a097cde02de7a2da29dfd971903c528aa
Artem <artem2325@bk.ru>
2019-04-25 21:24:16 +0300

    1

---
+++ src/sdobriyan/Task2.java
@@ -0,0 +1,11 @@
+package sdobriyan;
+
+public class Task2 {
+    public static void main (String [] args) {
+        System.out.println ("Я начинаю изучать Java");
+        System.out.println ("Я начинаю изучать Java");
+        System.out.println ("Я начинаю изучать Java");
+        System.out.println ("Я начинаю изучать Java");
+        System.out.println ("Я начинаю изучать Java");
+    }
+}
