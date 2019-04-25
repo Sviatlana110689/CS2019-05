da731803e0fb71f2945b4c6daec12b729a7f8c2a
Artem <artem2325@bk.ru>
2019-04-25 21:32:50 +0300

    Task 3 (A) - complete

---
+++ src/sdobriyan/Task3.java
@@ -0,0 +1,10 @@
+package sdobriyan;
+
+public class Task3 {
+    public static void main (String [] args) {
+        int i = 3;
+        int j = 4;
+        int k = 25;
+        System.out.println(i + "*" + i + "+" + j + "*" + j + "=" + k);
+    }
+}
